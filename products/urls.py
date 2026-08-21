2376821aa2660ee6eb49947fc3771d5e9aec5995
SureshKumar-2007 <sj4014917@gmail.com>
2026-08-21 08:10:32 +0530

    Initial commit

---
+++ products/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.product_list, name='product_list'),
+    path('update/<int:id>/', views.product_update, name = 'product_update'),
+    path('delete/<int:id>/', views.product_delete, name = 'prduct_delete'),
+]
