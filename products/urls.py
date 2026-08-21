bbc04cdfc09268a7f1013db8a29762cdf1301edb
SureshKumar-2007 <sj4014917@gmail.com>
2026-08-21 08:20:46 +0530

    Fixing error

--- products/urls.py
+++ products/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.product_list, name='product_list'),
     path('update/<int:id>/', views.product_update, name = 'product_update'),
-    path('delete/<int:id>/', views.product_delete, name = 'prduct_delete'),
+    path('delete/<int:id>/', views.product_delete, name = 'product_delete'),
 ]
