93d09d69f7f8f713c4ac5b3d846e3b43da92aa46
SureshKumar-2007 <sj4014917@gmail.com>
2026-08-21 08:26:10 +0530

    views file changes

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 # from django.shortcuts import render
 
 # Create your views here.
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect,  get_object_or_404
 from .models import Product
 from .forms import ProductForm
 
